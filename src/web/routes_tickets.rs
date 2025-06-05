86921bfbd00a513bc09fa0f623fb6694e5c988a3
Ken Rutan <divinewraith@DivineWraiths-MacBook-Pro.local>
2025-06-05 01:13:46 -0700

    added further modifications to ctx.

--- src/web/routes_tickets.rs
+++ src/web/routes_tickets.rs
@@ -1,5 +1,6 @@
 use crate::model::{ModelController, Ticket, TicketForCreate};
 use crate::Result;
+use crate::ctx::Ctx;
 use axum::extract::{Path, State};
 use axum::routing::{delete, post, get, put};
 use axum::{Json, Router};
@@ -7,51 +8,57 @@ use axum::{Json, Router};
 pub fn routes(mc: ModelController) -> Router {
 	Router::new()
 		.route("/tickets", post(create_ticket).get(list_tickets))
-		.route("/tickets/:id", put(update_ticket).delete(delete_ticket))
+		.route("/tickets/:id", delete(delete_ticket))
 		.with_state(mc)
 }
 
 // region:    --- REST Handlers
 async fn create_ticket(
 	State(mc): State<ModelController>,
+	ctx: Ctx,
 	Json(ticket_fc): Json<TicketForCreate>,
 ) -> Result<Json<Ticket>> {
 	println!("->> {:<12} - create_ticket", "HANDLER");
 
-	let ticket = mc.create_ticket(ticket_fc).await?;
+	let ticket = mc.create_ticket(ctx, ticket_fc).await?;
 
 	Ok(Json(ticket))
 }
 
 async fn list_tickets(
 	State(mc): State<ModelController>,
+	ctx: Ctx,
 ) -> Result<Json<Vec<Ticket>>> {
 	println!("->> {:<12} - list_tickets", "HANDLER");
 
-	let tickets = mc.list_tickets().await?;
+	let tickets = mc.list_tickets(ctx).await?;
 
 	Ok(Json(tickets))
 }
 
-async fn update_ticket(
+// Getting this to work right is a low priority now.
+/*
+ async fn update_ticket(
 	State(mc): State<ModelController>, Path(id): Path<u64>,
+	ctx: Ctx,
 	Json(ticket_fc): Json<TicketForCreate>,
 ) -> Result<Json<Ticket>> {
 	println!("->> {:<12} - update_ticket", "HANDLER");
 
-    let ticket = mc.delete_ticket(id).await?;
-    let ticket = mc.create_ticket(ticket_fc).await?;
+    let ticket = mc.delete_ticket(ctx, id).await?;
+    let ticket = mc.create_ticket(ctx, ticket_fc).await?;
     
     Ok(Json(ticket))
-}
+}*/
 
 async fn delete_ticket(
 	State(mc): State<ModelController>,
+	ctx: Ctx,
 	Path(id): Path<u64>,
 ) -> Result<Json<Ticket>> {
 	println!(">>> {:<12} - delete_ticket", "HANDLER");
 
-	let ticket = mc.delete_ticket(id).await?;
+	let ticket = mc.delete_ticket(ctx, id).await?;
 
 	Ok(Json(ticket))
 }
