62d6c7e30e543d59aa319cf6d3144cac524939bd
Ken Rutan <divinewraith@DivineWraiths-MacBook-Pro.local>
2025-06-09 02:32:07 -0700

    added list and create database commands.

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,5 +1,6 @@
 use super::db::Db;
 use crate::model;
+use sqlb::HasFields;
 
 // region: Todo Types
 #[derive(sqlx::FromRow, Debug, Clone)]
@@ -10,9 +11,9 @@ pub struct Todo {
     pub status: TodoStatus,
 }
 
-#[derive(Default, Clone)]
+#[derive(sqlb::Fields, Default, Debug, Clone)]
 pub struct TodoPatch {
-    pub cid: Option<i64>,
+    //pub cid: Option<i64>,
     pub title: Option<String>,
     pub status: Option<TodoStatus>,
 }
@@ -24,6 +25,7 @@ pub enum TodoStatus {
     Open,
     Close,
 }
+sqlb::bindable!(TodoStatus);
 // endregion: Todo Types
 
 
@@ -32,21 +34,28 @@ pub struct TodoMac;
 
 impl TodoMac {
     pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error2> {
-        let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status";
-        let query = sqlx::query_as::<_, Todo>(&sql).bind(123 as i64).bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+        //let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status";
+        //let query = sqlx::query_as::<_, Todo>(&sql).bind(123 as i64).bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+        let mut fields = data.fields();
+        fields.push(("cid", 123).into());
+        let sb = sqlb::insert().table("todo").data(fields).returning(&["id", "cid", "title", "status"]);
 
-        let todo = query.fetch_one(db).await?;
+        // Execute the query
+        //let todo = query.fetch_one(db).await?;
+        let todo = sb.fetch_one(db).await?;
 
         Ok(todo)
     }
 
     pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error2> {
-        let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+        //let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+        let sb = sqlb::select().table("todo").columns(&["id", "cid", "title", "status"]).order_by("!id");
 
         // Build the sqlx-querey
-        let query = sqlx::query_as(&sql);
+        //let query = sqlx::query_as(&sql);
         // Execute the query
-        let todos = query.fetch_all(db).await?;
+        //let todos = query.fetch_all(db).await?;
+        let todos = sb.fetch_all(db).await?;
 
         Ok(todos)
     }
